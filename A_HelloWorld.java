a3a7abed973d42fc258b024680a82122b7b5ae1f
Yogeshgj24 <101520390+Yogeshgj24@users.noreply.github.com>
2022-07-02 17:03:06 +0530

    Add files via upload

---
+++ A_HelloWorld.java
@@ -0,0 +1,28 @@
+package com.my_pkg;
+
+//is class is created by Yogesh
+// this is comment in java
+public class A_HelloWorld {
+	
+	public void myMethod() {
+		
+	}
+	
+	public int mySecondMethd() {
+		
+		
+		return 10;
+	}
+	
+	
+
+	public  void main(String[] args) {
+		
+		
+		int i = 0; // this veriable is initialised to run for loop
+		
+		System.out.println("hi............");
+
+	}
+
+}
---
+++ DeleteThisClass.java
@@ -0,0 +1,5 @@
+package myFolder;
+
+public class DeleteThisClass {
+
+}
